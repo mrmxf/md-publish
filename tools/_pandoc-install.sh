74f566827e2d56f422f20e04b62bb7a875af0dce
Mr MXF <bruce@mrmxf.com>
2022-01-12 13:28:46 +0000

    echo pandoc install version

--- tools/_pandoc-install.sh
+++ tools/_pandoc-install.sh
@@ -1,7 +1,9 @@
 # install pandoc on a linux system
-DEB="pandoc-2.16.1-1-amd64.deb"
+DEB="zmp/tools/pandoc-amd64.deb"
 GOBACK="$(pwd)"
-cd /tmp
-curl -L https://github.com/jgm/pandoc/releases/download/2.16.1/$DEB --output $DEB
+# cd /tmp
+# curl -L https://github.com/jgm/pandoc/releases/download/2.16.1/$DEB --output $DEB
 sudo dpkg -i $DEB
-cd $GOBACK
+#cd $GOBACK
+echo -e "${Ctxt}Installed ${Ccmd}$(pandoc --version | awk '{print $1" "$2" "$3; exit}')$Coff"
+exit 0
